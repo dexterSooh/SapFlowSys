c44cf9b341f21a11d4f5e98a4691131dced37887
EOM <tomoru55.tom@gmail.com>
2022-05-23 00:11:20 +0900

    frmLogin.Designer.cs 구버전으로 롤백

--- WindowsFormsApplication1/frmLogin.Designer.cs
+++ WindowsFormsApplication1/frmLogin.Designer.cs
@@ -28,138 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label3 = new ReaLTaiizor.Controls.MaterialLabel();
-            this.label2 = new ReaLTaiizor.Controls.MaterialLabel();
-            this.label1 = new ReaLTaiizor.Controls.MaterialLabel();
-            this.txbPassword = new ReaLTaiizor.Controls.MaterialTextBox();
-            this.txbID = new ReaLTaiizor.Controls.MaterialTextBox();
-            this.materialButton1 = new ReaLTaiizor.Controls.MaterialButton();
-            this.materialButton2 = new ReaLTaiizor.Controls.MaterialButton();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.txbID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(101, 90);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(52, 22);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(14, 90);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(57, 22);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Depth = 0;
-            this.label3.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.label3.Location = new System.Drawing.Point(132, 158);
+            this.label3.Location = new System.Drawing.Point(12, 58);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 19);
+            this.label3.Size = new System.Drawing.Size(23, 12);
             this.label3.TabIndex = 4;
             this.label3.Text = "PW";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Depth = 0;
-            this.label2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.label2.Location = new System.Drawing.Point(138, 115);
+            this.label2.Location = new System.Drawing.Point(12, 40);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 19);
+            this.label2.Size = new System.Drawing.Size(16, 12);
             this.label2.TabIndex = 5;
             this.label2.Text = "ID";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Depth = 0;
-            this.label1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.label1.Location = new System.Drawing.Point(165, 73);
+            this.label1.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.Size = new System.Drawing.Size(110, 16);
             this.label1.TabIndex = 6;
             this.label1.Text = "Real login";
             // 
             // txbPassword
             // 
-            this.txbPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txbPassword.Depth = 0;
-            this.txbPassword.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.txbPassword.Location = new System.Drawing.Point(161, 151);
+            this.txbPassword.Location = new System.Drawing.Point(70, 56);
             this.txbPassword.Margin = new System.Windows.Forms.Padding(2);
-            this.txbPassword.MaxLength = 50;
-            this.txbPassword.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.OUT;
-            this.txbPassword.Multiline = false;
             this.txbPassword.Name = "txbPassword";
-            this.txbPassword.Password = true;
-            this.txbPassword.Size = new System.Drawing.Size(107, 36);
+            this.txbPassword.PasswordChar = '*';
+            this.txbPassword.Size = new System.Drawing.Size(83, 21);
             this.txbPassword.TabIndex = 1;
-            this.txbPassword.Text = "";
-            this.txbPassword.UseTallSize = false;
+            this.txbPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbPassword_KeyPress);
             // 
             // txbID
             // 
-            this.txbID.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txbID.Depth = 0;
-            this.txbID.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.txbID.Location = new System.Drawing.Point(161, 105);
+            this.txbID.Location = new System.Drawing.Point(70, 33);
             this.txbID.Margin = new System.Windows.Forms.Padding(2);
-            this.txbID.MaxLength = 50;
-            this.txbID.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.OUT;
-            this.txbID.Multiline = false;
             this.txbID.Name = "txbID";
+            this.txbID.PasswordChar = '*';
             this.txbID.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.txbID.Size = new System.Drawing.Size(107, 36);
+            this.txbID.Size = new System.Drawing.Size(83, 21);
             this.txbID.TabIndex = 0;
-            this.txbID.Text = "";
-            this.txbID.UseTallSize = false;
-            // 
-            // materialButton1
-            // 
-            this.materialButton1.AutoSize = false;
-            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.materialButton1.Depth = 0;
-            this.materialButton1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.materialButton1.DrawShadows = true;
-            this.materialButton1.HighEmphasis = true;
-            this.materialButton1.Icon = null;
-            this.materialButton1.Location = new System.Drawing.Point(209, 207);
-            this.materialButton1.Margin = new System.Windows.Forms.Padding(2);
-            this.materialButton1.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
-            this.materialButton1.Name = "materialButton1";
-            this.materialButton1.Size = new System.Drawing.Size(59, 29);
-            this.materialButton1.TabIndex = 3;
-            this.materialButton1.Text = "Cancel";
-            this.materialButton1.TextState = ReaLTaiizor.Controls.MaterialButton.TextStateType.Normal;
-            this.materialButton1.Type = ReaLTaiizor.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.materialButton1.UseAccentColor = false;
-            this.materialButton1.UseVisualStyleBackColor = true;
-            this.materialButton1.Click += new System.EventHandler(this.materialButton1_Click);
-            // 
-            // materialButton2
-            // 
-            this.materialButton2.AutoSize = false;
-            this.materialButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.materialButton2.Depth = 0;
-            this.materialButton2.DrawShadows = true;
-            this.materialButton2.HighEmphasis = true;
-            this.materialButton2.Icon = null;
-            this.materialButton2.Location = new System.Drawing.Point(135, 207);
-            this.materialButton2.Margin = new System.Windows.Forms.Padding(2);
-            this.materialButton2.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
-            this.materialButton2.Name = "materialButton2";
-            this.materialButton2.Size = new System.Drawing.Size(59, 29);
-            this.materialButton2.TabIndex = 2;
-            this.materialButton2.Text = "OK";
-            this.materialButton2.TextState = ReaLTaiizor.Controls.MaterialButton.TextStateType.Normal;
-            this.materialButton2.Type = ReaLTaiizor.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.materialButton2.UseAccentColor = false;
-            this.materialButton2.UseVisualStyleBackColor = true;
-            this.materialButton2.Click += new System.EventHandler(this.materialButton2_Click);
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 255);
-            this.Controls.Add(this.materialButton2);
-            this.Controls.Add(this.materialButton1);
+            this.ClientSize = new System.Drawing.Size(164, 123);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.txbPassword);
             this.Controls.Add(this.txbID);
             this.Controls.Add(this.label3);
@@ -180,12 +138,13 @@
         }
 
         #endregion
-        private ReaLTaiizor.Controls.MaterialTextBox txbPassword;
-        private ReaLTaiizor.Controls.MaterialTextBox txbID;
-        private ReaLTaiizor.Controls.MaterialLabel label3;
-        private ReaLTaiizor.Controls.MaterialLabel label2;
-        private ReaLTaiizor.Controls.MaterialLabel label1;
-        private ReaLTaiizor.Controls.MaterialButton materialButton1;
-        private ReaLTaiizor.Controls.MaterialButton materialButton2;
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.TextBox txbID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- WindowsFormsApplication1/frmLogin.cs
+++ WindowsFormsApplication1/frmLogin.cs
@@ -1,187 +1,179 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Diagnostics;
-using ReaLTaiizor.Forms;
-using Flurl;
+﻿using Flurl;
 using Flurl.Http;
-using SapflowApplication.Models;
-using System.Net;
-using SapflowApplication.Helpers;
 using Newtonsoft.Json;
+using SapflowApplication.Helpers;
+using SapflowApplication.Models;
+using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Net;
+using System.Windows.Forms;
 
 namespace SapflowApplication
 {
-    public partial class frmLogin : MaterialForm
-    {
-        public frmLogin()
-        {
-            InitializeComponent();
-        }
+	public partial class frmLogin : Form
+	{
+		public frmLogin()
+		{
+			InitializeComponent();
+		}
 
-        //public string ID
-        //{
-        //    get
-        //    {
-        //        return (txbID.Text);
-        //    }
-        //}
+		//public string ID
+		//{
+		//    get
+		//    {
+		//        return (txbID.Text);
+		//    }
+		//}
 
-        //public string Password
-        //{
-        //    get
-        //    {
-        //        return (txbPassword.Text);
-        //    }
-        //}
+		//public string Password
+		//{
+		//    get
+		//    {
+		//        return (txbPassword.Text);
+		//    }
+		//}
 
-        private async void btnOK_Click(object sender, EventArgs e)
-        {
-            AuthenticationResponse response;
-            try
-            {
-                response = await ApiPaths.BaseUrl
-                    .AppendPathSegment(ApiPaths.Authenticate)
-                    .PostJsonAsync(new AuthenticationQuery
-                    {
-                        Id = txbID.Text.Trim(),
-                        Password = txbPassword.Text.Trim()
-                    }).ReceiveJson<AuthenticationResponse>();
+		private async void btnOK_Click(object sender, EventArgs e)
+		{
+			AuthenticationResponse response;
+			try
+			{
+				response = await ApiPaths.BaseUrl
+					.AppendPathSegment(ApiPaths.Authenticate)
+					.PostJsonAsync(new AuthenticationQuery
+					{
+						Id = txbID.Text.Trim(),
+						Password = txbPassword.Text.Trim()
+					}).ReceiveJson<AuthenticationResponse>();
 
-                AppConfiguration.SetAppConfig("accessToken", response.Token);
-                ConfigurationManager.RefreshSection("appSettings");
-            }
-            catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.BadRequest)
-            {
-                MessageBox.Show("invalid token");
-            }
-            catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
-            {
-                var error = await ex.GetResponseJsonAsync<BaseException>();
-                var description = JsonConvert.DeserializeObject<Dictionary<string, string>>(error.Description);
-                MessageBox.Show($"{error.Message} (id: {description["Id"]})");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+				AppConfiguration.SetAppConfig("accessToken", response.Token);
+				ConfigurationManager.RefreshSection("appSettings");
+			}
+			catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.BadRequest)
+			{
+				MessageBox.Show("invalid token");
+			}
+			catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+			{
+				var error = await ex.GetResponseJsonAsync<BaseException>();
+				var description = JsonConvert.DeserializeObject<Dictionary<string, string>>(error.Description);
+				MessageBox.Show($"{error.Message} (id: {description["Id"]})");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 
-            //if (ID == "real")
-            //{
-            //    if(Password=="real")
-            //    {
-            //        Debug.WriteLine("118");
-            //    }
-            //    string passwd ="";
+			//if (ID == "real")
+			//{
+			//    if(Password=="real")
+			//    {
+			//        Debug.WriteLine("118");
+			//    }
+			//    string passwd ="";
 
-            //    bool fgPasswordMatched = true;
-            //    if (Password.Length >= 6)
-            //    {
-            //        for (int i = 0; i < 6; i++)
-            //        {
-            //            if (Password.Substring(i, 1) != (i + 1).ToString())
-            //            {
-            //                fgPasswordMatched = false;
-            //            }
-            //        }
-            //    }
-            //    else
-            //    {
-            //        fgPasswordMatched = false;
-            //    }
+			//    bool fgPasswordMatched = true;
+			//    if (Password.Length >= 6)
+			//    {
+			//        for (int i = 0; i < 6; i++)
+			//        {
+			//            if (Password.Substring(i, 1) != (i + 1).ToString())
+			//            {
+			//                fgPasswordMatched = false;
+			//            }
+			//        }
+			//    }
+			//    else
+			//    {
+			//        fgPasswordMatched = false;
+			//    }
 
-            //    if (fgPasswordMatched)
-            //    {
-            //        SF_Test.fgActivateFactorySetup = true;
-            //    }
-            //    if (Password == "address")
-            //    {
-            //        Debug.WriteLine("08826");
-            //    }
+			//    if (fgPasswordMatched)
+			//    {
+			//        SF_Test.fgActivateFactorySetup = true;
+			//    }
+			//    if (Password == "address")
+			//    {
+			//        Debug.WriteLine("08826");
+			//    }
 
-            //}
+			//}
 
-            try
-            {
-                this.DialogResult = DialogResult.OK;
-                //this.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "[ERROR]", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+			try
+			{
+				this.DialogResult = DialogResult.OK;
+				//this.Close();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "[ERROR]", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            this.DialogResult = DialogResult.Cancel;
-        }
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.Cancel;
+		}
 
-        private void frmLogin_Load(object sender, EventArgs e)
-        {
-            txbID.Select();
-        }
+		private void frmLogin_Load(object sender, EventArgs e)
+		{
+			txbID.Select();
+		}
 
-        private void txbPassword_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            //if (e.KeyChar == '\r')
-            //{
-            //    if (txbPassword.Text != "")
-            //    {
-            //        try
-            //        {
-            //            btnOK_Click(sender, e);
-            //        }
-            //        catch (Exception ex) { Debug.WriteLine(ex.Message.ToString()); }
-            //    }
-            //}
+		private void txbPassword_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//if (e.KeyChar == '\r')
+			//{
+			//    if (txbPassword.Text != "")
+			//    {
+			//        try
+			//        {
+			//            btnOK_Click(sender, e);
+			//        }
+			//        catch (Exception ex) { Debug.WriteLine(ex.Message.ToString()); }
+			//    }
+			//}
 
-        }
+		}
 
-        private void materialButton1_Click(object sender, EventArgs e)
-        {
-            this.DialogResult = DialogResult.Cancel;
-        }
+		private void materialButton1_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.Cancel;
+		}
 
-        private async void materialButton2_Click(object sender, EventArgs e)
-        {
-            //AuthenticationResponse response;
-            //try
-            //{
-            //    response = await ApiPaths.BaseUrl
-            //        .AppendPathSegment(ApiPaths.Authenticate)
-            //        .PostJsonAsync(new AuthenticationQuery
-            //        {
-            //            Id = txbID.Text.Trim(),
-            //            Password = txbPassword.Text.Trim()
-            //        }).ReceiveJson<AuthenticationResponse>();
+		private async void materialButton2_Click(object sender, EventArgs e)
+		{
+			//AuthenticationResponse response;
+			//try
+			//{
+			//    response = await ApiPaths.BaseUrl
+			//        .AppendPathSegment(ApiPaths.Authenticate)
+			//        .PostJsonAsync(new AuthenticationQuery
+			//        {
+			//            Id = txbID.Text.Trim(),
+			//            Password = txbPassword.Text.Trim()
+			//        }).ReceiveJson<AuthenticationResponse>();
 
-            //    AppConfiguration.SetAppConfig("accessToken", response.Token);
-            //    //ConfigurationManager.RefreshSection("appSettings");
-            //}
-            //catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.BadRequest)
-            //{
-            //    MessageBox.Show("invalid token");
-            //}
-            //catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
-            //{
-            //    var error = await ex.GetResponseJsonAsync<BaseException>();
-            //    var description = JsonConvert.DeserializeObject<Dictionary<string, string>>(error.Description);
-            //    MessageBox.Show($"{error.Message} (id: {description["Id"]})");
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
+			//    AppConfiguration.SetAppConfig("accessToken", response.Token);
+			//    //ConfigurationManager.RefreshSection("appSettings");
+			//}
+			//catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.BadRequest)
+			//{
+			//    MessageBox.Show("invalid token");
+			//}
+			//catch (FlurlHttpException ex) when (ex.Call.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+			//{
+			//    var error = await ex.GetResponseJsonAsync<BaseException>();
+			//    var description = JsonConvert.DeserializeObject<Dictionary<string, string>>(error.Description);
+			//    MessageBox.Show($"{error.Message} (id: {description["Id"]})");
+			//}
+			//catch (Exception ex)
+			//{
+			//    MessageBox.Show(ex.Message);
+			//}
 
-            this.DialogResult = DialogResult.OK;
-        }
-    }
+			this.DialogResult = DialogResult.OK;
+		}
+	}
 }
